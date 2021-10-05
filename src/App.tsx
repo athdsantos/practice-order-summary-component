c5f0ecb081c14db71a73ab8da0578e800077a956
Athos Henrique <athdsantos@gmail.com>
2021-10-05 20:32:14 -0300

    upload projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import iconMusic from './images/icon-music.svg'
+import illustrationHero from './images/illustration-hero.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={illustrationHero} className="imgHeader" alt="Backgound header Illustration Hero" />
+      <h1>Order Summary</h1>
+      <p>You can now listen to millions of songs, audibooks, and podcasts on any devices anywhere you like!</p>
+      <div className="plan">
+        <img src={iconMusic} alt="Icon Music" />
+        <p><strong>Anual Plan</strong><br />$59.99/year</p>
+        <a href="/">Change</a>
+      </div>
+      <a href="/" className="buttons proceed">Proceed to payment</a>
+      <a href="/" className="buttons cancel">Cancel order</a>
     </div>
   );
 }
